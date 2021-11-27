9cc011e76e2e2a1e19076f7a4acff09e6acf8d74
luet001 <http://git.dev.cmrh.com/>
2021-11-28 00:12:19 +0800

    fourth

--- parallel.c
+++ parallel.c
@@ -1,14 +1,52 @@
 /*
  * Simple parallel program to test for percolation of a cluster
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
 #include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include "arralloc.h"
 
 #include "percolate.h"
 
+
+int compareFile(FILE * fPtr1, FILE * fPtr2, int * line, int * col)
+{
+    char ch1, ch2;
+
+    *line = 1;
+    *col  = 0;
+
+    do
+    {
+        // Input character from both files
+        ch1 = fgetc(fPtr1);
+        ch2 = fgetc(fPtr2);
+
+        // Increment line
+        if (ch1 == '\n')
+        {
+            *line += 1;
+            *col = 0;
+        }
+
+        // If characters are not same then return -1
+        if (ch1 != ch2)
+            return -1;
+
+        *col  += 1;
+
+    } while (ch1 != EOF && ch2 != EOF);
+
+
+    /* If both files have reached end */
+    if (ch1 == EOF && ch2 == EOF)
+        return 0;
+    else
+        return -1;
+}
 /*
  * Simple parallel program to test for percolation of a cluster.
  */
@@ -20,22 +58,30 @@ int main(int argc, char *argv[])
    */
   clock_t t;
   t = clock();
-  int old[M+2][N+2], new[M+2][N+2];
+  int L;
+  L = atoi(argv[2]);
+  int NPROC;
+  NPROC = atoi(argv[3]);
+  int M = L/NPROC;
+  int N = L;
+  int **old, **new;
+  //int old[M+2][N+2], new[M+2][N+2];
 
   /*
    *  Additional array WITHOUT halos for initialisation and IO. This
    *  is of size LxL because, even in our parallel program, we do
    *  these two steps in serial
    */
-
-  int map[L][L];
-  int maptmp[L][L];
+  int **map;
+  //int map[L][L];
+  int **maptmp;
+  //int maptmp[L][L];
 
   /*
    *  Array to store local part of map
    */
-
-  int smallmap[M][N];
+  int **smallmap;
+  //int smallmap[M][N];
 
   /*
    *  Variables that define the simulation
@@ -102,31 +148,45 @@ int main(int argc, char *argv[])
 		 NPROC, size);
 	}
 
-      MPI_Finalize();
-      return 0;
+      // MPI_Finalize();
+      // return 0;
   }
 
-  if (argc != 2)
+  if (argc != 4)
     {
       if (rank == 0)
 	{
-	  printf("Usage: percolate <seed>\n");
+	  printf("Usage: percolate <seed> <size> <NPROC>\n");
 	}
 
       MPI_Finalize();
       return 0;
     }
+  old = (int **) arralloc(sizeof(int), 2, M+2, N+2);
+  new = (int **) arralloc(sizeof(int), 2, M+2, N+2);
+  map = (int **) arralloc(sizeof(int), 2, L, L);
+  maptmp = (int **) arralloc(sizeof(int), 2, L, L);
+  smallmap = (int **) arralloc(sizeof(int), 2, M, N);
+  if (NULL == old || NULL == new || NULL == map || NULL == maptmp || NULL == smallmap)
+{
+  printf("percolate: array allocation failed\n");
+  return 1;
+}
 
   /*
    *  Update for a fixed number of steps, periodically report progress
    */
 
   maxstep = 5*L;
+  if (L < 100){
+    maxstep = 5 * 100;
+  }
   printfreq = 100;
+
 
   if (rank == 0)
     {
-      printf("percolate: running on %d process(es)\n", size);
+      printf("percolate: running on %d process(es)\n", NPROC);
 
       /*
        *  Set most important value: the rock density rho (between 0 and 1)
@@ -154,23 +214,23 @@ int main(int argc, char *argv[])
       nhole = 0;
 
       for (i=0; i < L; i++)
-	{
-	  for (j=0; j < L; j++)
-	    {
-	      r=uni();
+    	{
+    	  for (j=0; j < L; j++)
+    	    {
+    	      r=uni();
 
-	      if(r < rho)
-		{
-		  map[i][j] = 0;
-		}
-	      else
-		{
-		  nhole++;
-		  map[i][j] = nhole;
-		}
-	    }
-	}
-
+    	      if(r < rho)
+        		{
+        		  map[i][j] = 0;
+        		}
+    	      else
+        		{
+        		  nhole++;
+        		  map[i][j] = nhole;
+        		}
+    	    }
+    	}
+      printf("n hole is %d\n",nhole);
       printf("percolate: rho = %f, actual density = %f\n",
 	      rho, 1.0 - ((double) nhole)/((double) L*L) );
     }
@@ -201,9 +261,9 @@ int main(int argc, char *argv[])
   for (i=1; i <= M; i++)
     {
       for (j=1; j <= N; j++)
-	{
-	  old[i][j] = smallmap[i-1][j-1];
-	}
+    	{
+    	  old[i][j] = smallmap[i-1][j-1];
+    	}
     }
 
   /*
@@ -250,7 +310,7 @@ int main(int argc, char *argv[])
 		   &old[0][1], N, MPI_INT, prev, tag,
 		   comm, &status);
 
-      MPI_Sendrecv(&old[1][1],   N, MPI_INT, prev, tag,
+      MPI_Sendrecv(&old[1][1], N, MPI_INT, prev, tag,
 		   &old[M+1][1], N, MPI_INT, next, tag,
 		   comm, &status);
 
@@ -269,21 +329,21 @@ int main(int argc, char *argv[])
 	       */
 
 	      if (oldval != 0)
-		{
-		  if (old[i][j-1] > newval) newval = old[i][j-1];
-		  if (old[i][j+1] > newval) newval = old[i][j+1];
-		  if (old[i-1][j] > newval) newval = old[i-1][j];
-		  if (old[i+1][j] > newval) newval = old[i+1][j];
+    		{
+    		  if (old[i][j-1] > newval) newval = old[i][j-1];
+    		  if (old[i][j+1] > newval) newval = old[i][j+1];
+    		  if (old[i-1][j] > newval) newval = old[i-1][j];
+    		  if (old[i+1][j] > newval) newval = old[i+1][j];
 
-		  if (newval != oldval)
-		    {
-		      ++nchangelocal;
-		    }
-		}
+    		  if (newval != oldval)
+    		    {
+    		      ++nchangelocal;
+    		    }
+    		}
 
-	      new[i][j] = newval;
-	    }
-	}
+    	      new[i][j] = newval;
+    	    }
+    	}
 
       /*
        *  Compute global number of changes on rank 0
@@ -296,28 +356,48 @@ int main(int argc, char *argv[])
        */
 
       if (step % printfreq == 0)
-	{
-	  if (rank == 0)
-	    {
-              printf("percolate: changes on step %d is %d\n",
-                     step, nchange);
-	    }
-	}
+    	{
+    	  if (rank == 0)
+    	    {
+                  printf("percolate: changes on step %d is %d\n",
+                         step, nchange);
+    	    }
+    	}
 
       /*
        *  Copy back in preparation for next step, omitting halos
        */
-
+      int total;
+      total = 0;
       for (i=1; i<=M; i++)
-	{
-	  for (j=1; j<=N; j++)
-	    {
-	      old[i][j] = new[i][j];
-	    }
-	}
+    	{
+    	  for (j=1; j<=N; j++)
+    	    {
+            total += new[i][j];
+    	      old[i][j] = new[i][j];
+
+    	    }
+    	}
+      if (step % printfreq == 0)
+      {
+        if (rank == 0)
+          {
+                  printf("Average value of map array on step %d is %d\n",
+                         step, total/M/N);
+          }
+      }
 
       step++;
+      // if (rank == 0 && nchange == 0 && step >= maxstep/2){
+      //   printf("???");
+      //   step = maxstep + 1;
+      // }
     }
+  if(rank == 0){
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    printf("fun() took %f seconds to execute. Average time for step is %f \n", time_taken, time_taken/maxstep);
+  }
 
   /*
    *  We set a maximum number of steps to ensure the algorithm always
@@ -325,14 +405,13 @@ int main(int argc, char *argv[])
    *  has finished then there must have been a problem (e.g. the value
    *  of maxstep is too small)
    */
-
   if (rank == 0)
     {
       if (nchange != 0)
-	{
-          printf("percolate: WARNING max steps = %d reached but nchange != 0\n",
-	     maxstep);
-	}
+    	{
+        printf("percolate: WARNING max steps = %d reached but nchange != 0\n",
+    	     maxstep);
+    	}
     }
 
   /*
@@ -342,9 +421,9 @@ int main(int argc, char *argv[])
   for (i=1; i<=M; i++)
     {
       for (j=1; j<=N; j++)
-	{
-	  smallmap[i-1][j-1] = old[i][j];
-	}
+    	{
+    	  smallmap[i-1][j-1] = old[i][j];
+    	}
     }
 
   /*
@@ -418,12 +497,79 @@ int main(int argc, char *argv[])
        *  cleanest with only a single cluster, but multiple clusters
        *  are useful for debugging.
        */
-      t = clock() - t;
-      double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
-      printf("fun() took %f seconds to execute \n", time_taken);
 
-      mapwrite("map.pgm", map, 2);
+
+      mapwritedynamic("map.pgm", map, L, 2);
+
+      FILE *f = fopen("map_parallel.data", "wb");
+      for(int i=0;i<L;++i){
+        for(int j=0;j<L;++j){
+         fwrite((char*)&(map[i][j]),sizeof(char),sizeof(int), f);
+       }
+      }
+      // fwrite(map, sizeof(char), sizeof(map), f);
+      fclose(f);
+
+      /* File pointer to hold reference of input file */
+      FILE * fPtr1;
+      FILE * fPtr2;
+      char path1[100];
+      char path2[100];
+
+      int diff;
+      int line, col;
+
+
+      /* Input path of files to compare */
+      // printf("Enter path of first file: ");
+      // scanf("%s", path1);
+      // printf("Enter path of second file: ");
+      // scanf("%s", path2);
+      strcpy(path1, "map.data");
+      strcpy(path2, "map_parallel.data");
+
+
+      /*  Open all files to compare */
+      fPtr1 = fopen(path1, "r");
+      fPtr2 = fopen(path2, "r");
+
+      /* fopen() return NULL if unable to open file in given mode. */
+      if (fPtr1 == NULL || fPtr2 == NULL)
+      {
+          /* Unable to open file hence exit */
+          perror("Error");
+          printf("\nUnable to open file.\n");
+          printf("Please check whether file exists and you have read privilege.\n");
+          exit(EXIT_FAILURE);
+      }
+
+
+      /* Call function to compare file */
+      diff = compareFile(fPtr1, fPtr2, &line, &col);
+
+      if (diff == 0)
+      {
+          printf("\nBoth files are equal.\n");
+      }
+      else
+      {
+          printf("\nFiles are not equal.\n");
+          printf("Line: %d, col: %d\n", line, col);
+      }
+
+
+      /* Finally close files to release resources */
+      fclose(fPtr1);
+      fclose(fPtr2);
+
+
         }
+
+  free(old);
+  free(new);
+  free(map);
+  free(maptmp);
+  free(smallmap);
 
   MPI_Finalize();
 
